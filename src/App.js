e24577dd54feb273fa92788d01d96854b91140ab
Roberto <roberto.infante03@gmail.com>
2023-08-23 13:18:25 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./components/Home";
+
+function Projects() {
+    return <div>Projects Showcase</div>;
+}
+
+function Blog() {
+    return <div>Blog Page</div>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                {/* Navigation */}
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/projects">Projects</Link>
+                        </li>
+                        <li>
+                            <Link to="/blog">Blog</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                {/* Routes */}
+                <Routes>
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/blog" element={<Blog />} />
+                    <Route path="/*" element={<Home />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import "../App.css";
+import imageSrc from "../img/me_sea.jpg";
+
+// Function to send a message to the backend and retrieve the response
+async function sendMessageToBackend(userMessage) {
+    const response = await fetch("http://localhost:5000/api/chat", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message: userMessage }),
+    });
+    const data = await response.json();
+    return data.response; // This will be the SDK's response
+}
+
+function Home() {
+    const [chatOn, setChat] = useState(true);
+    const [messages, setMessages] = useState([]); // To store all chat messages
+
+    async function handleSendMessage(userMessage) {
+        const sdkResponse = await sendMessageToBackend(userMessage);
+        setMessages([
+            ...messages,
+            { sender: "user", text: userMessage },
+            { sender: "sdk", text: sdkResponse },
+        ]);
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className="profile-container">
+                    <img src={imageSrc} className="Profile-img" alt="logo" />
+                    <h2 className="text-3xl font-bold pt-6">Hello World!</h2>
+                    <p className="greeting pb-6">
+                        My name is Roberto and I'm a Information Scientist and
+                        Designer. Chat with my{" "}
+                        <span className="bot-name">bobby.AI</span> to know more
+                        about me!
+                    </p>
+                </div>
+            </header>
+
+            {chatOn && (
+                <section className="pt-10">
+                    {messages.map((message, index) => (
+                        <div
+                            key={index}
+                            className={`chat ${
+                                message.sender === "user"
+                                    ? "chat-end"
+                                    : "chat-start"
+                            }`}
+                        >
+                            <div className="chat-bubble">{message.text}</div>
+                        </div>
+                    ))}
+                    <div className="chat-input">
+                        <input
+                            type="text"
+                            placeholder="Type your message..."
+                            onKeyPress={(event) => {
+                                if (event.key === "Enter") {
+                                    handleSendMessage(event.target.value);
+                                    event.target.value = ""; // Clear the input
+                                }
+                            }}
+                        />
+                    </div>
+                </section>
+            )}
+        </div>
+    );
+}
+
+export default Home;
